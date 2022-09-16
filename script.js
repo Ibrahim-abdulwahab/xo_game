1c1a2efc896d7bbc588bc38cf1c50711d8a008b7
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-09-16 18:15:25 +0300

    winning and scoring algorithms work

--- script.js
+++ script.js
@@ -8,21 +8,60 @@ window.addEventListener('load', function(){
     const card7=document.getElementById("card-seven");
     const card8=document.getElementById("card-eight");
     const card9=document.getElementById("card-nine");
-    const scoreboard=document.getElementById("score");
-    let i=0;
+    const redscore=document.getElementById("score-red");
+    const yellowscore=document.getElementById("score-yellow");
+    let i=1;
+    let redpoint=1;
+    let yellowpoint=1;
 
     const playerorder=x=>{
         if (i%2==0){
-            playerturn="card-red"
+            playerturn="yellow"
         }
         else{
-            playerturn="card-yellow"
+            playerturn="red"
         }
         x.className=playerturn;
         i=i+1;
+        if (i>=4){
+            if((card1.className==playerturn && card2.className==playerturn && card3.className==playerturn)
+            ||(card4.className==playerturn && card5.className==playerturn && card6.className==playerturn)
+            || (card7.className==playerturn && card8.className==playerturn && card9.className==playerturn)
+            || (card1.className==playerturn && card4.className==playerturn && card7.className==playerturn)
+            || (card2.className==playerturn && card5.className==playerturn && card8.className==playerturn)
+            || (card3.className==playerturn && card6.className==playerturn && card9.className==playerturn)
+            || (card1.className==playerturn && card5.className==playerturn && card9.className==playerturn)
+            || (card7.className==playerturn && card5.className==playerturn && card3.className==playerturn)){
+                if (playerturn=="red"){
+                    redscore.innerHTML=`red: ${redpoint}`;
+                    redpoint=redpoint+1;
+                    i=1;
+                    card1.className="card";
+                    card2.className="card";
+                    card3.className="card";
+                    card4.className="card";
+                    card5.className="card";
+                    card6.className="card";
+                    card7.className="card";
+                    card8.className="card";
+                    card9.className="card";
+                }
+                else{yellowscore.innerHTML=`yellow ${yellowpoint}`;
+                    yellowpoint=yellowpoint+1;
+                    i=1;
+                    card1.className="card";
+                    card2.className="card";
+                    card3.className="card";
+                    card4.className="card";
+                    card5.className="card";
+                    card6.className="card";
+                    card7.className="card";
+                    card8.className="card";
+                    card9.className="card";}
+        }}
     }
         const card1click = () =>{
-           playerorder(card1)
+            playerorder(card1)
         }
         
         card1.addEventListener("click",card1click)
