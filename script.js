4524361434f30a7de46175b85b62ea11c62f7936
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-09-16 17:21:15 +0300

    added the players class in css

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 window.addEventListener('load', function(){
+    const card1=document.getElementById("card-one");
+    const card2=document.getElementById("card-two");
+    const card3=document.getElementById("card-three");
+    const card4=document.getElementById("card-four");
+    const card5=document.getElementById("card-five");
+    const card6=document.getElementById("card-six");
+    const card7=document.getElementById("card-seven");
+    const card8=document.getElementById("card-eight");
+    const card9=document.getElementById("card-nine");
+    const scoreboard=document.getElementById("score");
 
 
-
+    card1.addEventListener("click",card1click)
 })
